04eb159ecd6cd7f3cf03396774dfc67ad4f192e4
indominuShot <luang193@gmail.com>
2020-06-09 13:32:43 -0300

    inicial commit

---
+++ backend/src/server.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    return res.json({message: "success"});
+});
+
+app.listen(3333);
