b1c16c8153533fd52b323e9517e332123ecb2540
indominuShot <luang193@gmail.com>
2020-06-11 19:56:42 -0300

    mapa instroduzido; Listagem de itens do banco

--- frontend/src/pages/CreatePoint/index.tsx
+++ frontend/src/pages/CreatePoint/index.tsx
@@ -1,13 +1,39 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Link} from 'react-router-dom';
+import {TileLayer, Map, Marker} from 'react-leaflet';
 
 import { FiArrowLeft } from 'react-icons/fi';
+
+import api from '../../services/api';
 
 import logo from '../../assets/logo.svg';
 
 import './styles.css';
 
+
+/* ****** IMPORTANTE ******* */
+// Sempre que for criado um estado para um array ou objeto
+// Precisa ser informado qual tipo de variável será armazenada
+// Definir o corpo do objeto
+interface Item{
+    id : number,
+    title : string,
+    image_url : string
+}
+
 const CreatePoint = () => {
+    // <Item[]> atribui um argumento de tipagem ao estado, ou seja,
+    // Informa que o estado terá o formato de
+    //  um array de Item's deifnido acima
+    const [items, setItems] = useState<Item[]>([]);
+
+    useEffect(() => {
+        api.get('/items').then(response => {
+            setItems(response.data);
+        })
+    }, [])
+
+
     return (
         <div id="page-create-point">
             <header>
@@ -31,7 +57,7 @@ const CreatePoint = () => {
 
                     <div className="field">
                         <label htmlFor="name">Nome da entidade</label>
-                        <input 
+                        <input
                             type="text"
                             name="name"
                             id="name"/>
@@ -40,7 +66,7 @@ const CreatePoint = () => {
                     <div className="field-group">
                         <div className="field">
                             <label htmlFor="email">E-mail</label>
-                            <input 
+                            <input
                                 type="email"
                                 name="email"
                                 id="email"/>
@@ -48,7 +74,7 @@ const CreatePoint = () => {
 
                         <div className="field">
                             <label htmlFor="whatsapp">Whatsapp</label>
-                            <input 
+                            <input
                                 type="text"
                                 name="whatsapp"
                                 id="whatsapp"/>
@@ -64,6 +90,15 @@ const CreatePoint = () => {
                         <h2>Endereços</h2>
                         <span>Selecione o endereço no mapa</span>
                     </legend>
+
+                    <Map center={[-23.0737447,-48.9102096]} zoom={15}>
+                        <TileLayer
+                            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+
+                        <Marker position={[-23.0737447,-48.9102096]}/>
+                    </Map>
 
                     <div className="field-group">
 
@@ -94,36 +129,14 @@ const CreatePoint = () => {
                     </legend>
 
                     <ul className="items-grid">
-                        
-                        <li>
-                            <img src="http://localhost:3333/uploads/lampadas.svg" alt="teste"/>
-                            <span>Lâmpadas</span>
-                        </li>
-
-                        <li>
-                            <img src="http://localhost:3333/uploads/lampadas.svg" alt="teste"/>
-                            <span>Lâmpadas</span>
-                        </li>
-
-                        <li>
-                            <img src="http://localhost:3333/uploads/lampadas.svg" alt="teste"/>
-                            <span>Lâmpadas</span>
-                        </li>
-
-                        <li>
-                            <img src="http://localhost:3333/uploads/lampadas.svg" alt="teste"/>
-                            <span>Lâmpadas</span>
-                        </li>
-
-                        <li>
-                            <img src="http://localhost:3333/uploads/lampadas.svg" alt="teste"/>
-                            <span>Lâmpadas</span>
-                        </li>
-
-                        <li>
-                            <img src="http://localhost:3333/uploads/lampadas.svg" alt="teste"/>
-                            <span>Lâmpadas</span>
-                        </li>
+                        {
+                            items.map(item => (
+                                <li key={item.id}>
+                                    <img src={item.image_url} alt={item.title}/>
+                                    <span>{item.title}</span>
+                                </li>
+                            ))
+                        }
                     </ul>
                 </fieldset>
 
